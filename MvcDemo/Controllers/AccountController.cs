1151bb29b5db175b1ea715c5395df007f98b2201
ronak sharma <ronak.sharma@promactinfo.com>
2017-07-14 00:30:50 +0530

    code line2

--- MvcDemo/Controllers/AccountController.cs
+++ MvcDemo/Controllers/AccountController.cs
@@ -28,8 +28,10 @@ namespace MvcDemo.Controllers
 
         //code line1
 
+        //code line2
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
@@ -41,9 +43,9 @@ namespace MvcDemo.Controllers
             {
                 return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
             }
-            private set 
-            { 
-                _signInManager = value; 
+            private set
+            {
+                _signInManager = value;
             }
         }
 
@@ -130,7 +132,7 @@ namespace MvcDemo.Controllers
             // If a user enters incorrect codes for a specified amount of time then the user account 
             // will be locked out for a specified amount of time. 
             // You can configure the account lockout settings in IdentityConfig
-            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent:  model.RememberMe, rememberBrowser: model.RememberBrowser);
+            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent: model.RememberMe, rememberBrowser: model.RememberBrowser);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -166,7 +168,7 @@ namespace MvcDemo.Controllers
                 if (result.Succeeded)
                 {
                     var roleResult = UserManager.AddToRole(user.Id, "Admin");
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
