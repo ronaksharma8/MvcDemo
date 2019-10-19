cf84392b3db28021c43966105949a2ae182115ac
ronak <ronak.sharma@promactinfo.com>
2019-10-19 21:28:15 +0800

    adding a file at parent level and pushing directly to remote server without using the cmd

---
+++ MvcDemo/TestStudentResetSoft.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcDemo
+{
+    public class TestStudentResetSoft
+    {
+        public int Id { get; set; }
+    }
+}
