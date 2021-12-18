4487521f8d65c9fafd382b563c760acd9db16832
Ronak <ronak.sharma2209@gmail.com>
2021-12-18 08:52:07 +0800

    Msater direct

--- MvcDemo/Controllers/HelloWorldController.cs
+++ MvcDemo/Controllers/HelloWorldController.cs
@@ -12,6 +12,7 @@ namespace MvcDemo.Controllers
         // GET: /HelloWorld/ 
         // I am also adding the code from master branch directly from github let see how git fetch and git pull works
         // Adding one more commit master
+        //adding here
         public ActionResult Index()
         {
             return View();
